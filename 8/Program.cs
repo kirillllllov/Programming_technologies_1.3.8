d4c71e4b223e5c72764016d173fa2c9487e45cf2
koptelov.kir <koptelov.kir@mail.ru>
2025-02-20 18:40:21 +0800

    Добавьте файлы проекта.

---
+++ 8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Ввод предложения
+        Console.WriteLine("Введите предложение:");
+        string input = Console.ReadLine();
+        string output;
+
+        // Проверка на непустую строку
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Строка не должна быть пустой.");
+            return;
+        }
+
+        var result = Logic.CalculateLetterPercentage(input);
+
+        // Вывод результата
+        output = $"Доля букв в предложении: {result.percentage}%";
+        Console.WriteLine(output);
+    }
+}
+
+public class Logic
+{
+    public struct Result
+    {
+        public double percentage;
+    }
+
+    public static Result CalculateLetterPercentage(string input)
+    {
+        // Подсчёт общего количества символов и количества букв
+        int totalChars = input.Length;
+        int letterCount = input.Count(char.IsLetter);
+
+        // Вычисление процента букв
+        double percentage = (double)letterCount / totalChars * 100;
+
+        return new Result { percentage = Math.Round(percentage, 2) };
+    }
+}
---
+++ 8Tests/ProgramTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            string input = "53he";
+            var result = Logic.CalculateLetterPercentage(input);
+            string output = $"Доля букв в предложении: {result.percentage}%";
+            Assert.AreEqual(output, "Доля букв в предложении: 50%");
+            
+        }
+        public void MainTest2()
+        {
+            string input = "538787he";
+            var result = Logic.CalculateLetterPercentage(input);
+            string output = $"Доля букв в предложении: {result.percentage}%";
+            Assert.AreEqual(output, "Доля букв в предложении: 25%");
+
+        }
+    }
+}
